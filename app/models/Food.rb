a2ff4ae882ef9fe6ed5282477f519624945e528d
unknown <albertkantwi@gmail.com>
2023-11-30 15:31:56 +0000

    Change files name to fix error

--- app/models/Food.rb
+++ app/models/Food.rb
@@ -1,7 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
-  # has_many :recipes, through: :recipe_foods
 
   validates :name, presence: true, length: { maximum: 250 }
   validates :measurement_unit, presence: true
